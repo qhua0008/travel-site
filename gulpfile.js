ad65f464031a5d66b7ab9d50b2e7988d8518b753
Jennifer <qhua4556@gmail.com>
2019-03-28 18:55:35 +1100

    change file name and test

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,9 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+cssImport = require('postcss-import'),
+browserSync = require('browser-sync').create();
+
 
 gulp.task('default', function() {
   console.log("Hooray - you created a Gulp task.");
@@ -22,12 +24,24 @@ gulp.task('styles', function() {
 
 gulp.task('watch', function() {
 
+  browserSync.init({
+    server:{
+      baseDir: "app"
+    }
+  });
+
   watch('./app/index.html', function() {
-    gulp.start('html');
+    browserSync.reload();
   });
 
   watch('./app/assets/styles/**/*.css', function() {
-    gulp.start('styles');
+    gulp.start('cssInject');
   });
 
+});
+
+gulp.task('cssInject',['styles'], function(){
+  // dependencies: before doing cssInject, do the styles task first
+  return gulp.src('.app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
 });
